2d10b5a3585d51f9eabc0d33879ba92b9c92c900
goktug <goktugsultan@gmail.com>
2021-01-22 22:00:40 +0300

    add Navbar.js

---
+++ cheapshark-app/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function Navbar() {
+    return (
+        <div>
+            <nav className="navbar navbar-light bg-dark">
+                <a className="navbar-brand text-white mx-auto" href="#">Cheapest Game</a>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar;
